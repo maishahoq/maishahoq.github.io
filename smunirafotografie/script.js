6dd00fa822f34dc64bb66474954295a6b84d9d1b
Maisha Mahrin <maisha.mahrin30@gmail.com>
2023-12-07 14:44:57 +0400

    Base website

---
+++ smunirafotografie/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger-menu");
+  const menuLinks = document.querySelector(".menu-links");
+
+  hamburger.addEventListener("click", function () {
+    menuLinks.classList.toggle("active");
+  });
+});
